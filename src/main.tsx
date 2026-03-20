a7b5b9c70c03ffdb651eeec596c158e1619e0a99
Skip <skip@usecurling.com>
2026-03-20 00:07:58 +0000

    feat: Implement PWA installation and branding

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,18 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './main.css'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(
+      (registration) => {
+        console.log('ServiceWorker registration successful')
+      },
+      (err) => {
+        console.log('ServiceWorker registration failed: ', err)
+      },
+    )
+  })
+}
+
 // @skip-protected: Do not remove. Required for React rendering.
 createRoot(document.getElementById('root')!).render(<App />)
